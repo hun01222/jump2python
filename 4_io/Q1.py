b522bd0269736e783633223b456a676ca023e2f6
박지훈 <hun01222@naver.com>
2022-08-08 14:27:33 +0000

    new

---
+++ 4_io/Q1.py
@@ -0,0 +1,8 @@
+def is_odd(num):
+    if num % 2 == 0:
+        return False
+    else:
+        return True
+
+print(is_odd(2))
+print(is_odd(3))
---
+++ 4_io/Q6.py
@@ -0,0 +1,5 @@
+text = input()
+
+f1 = open("test_1.txt", "a")
+f1.write(text + "\n")
+f1.close()
